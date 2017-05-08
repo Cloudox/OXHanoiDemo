354b84389b7da233374fba68216e91a32e67c391
Cloudox <cloudox@foxmail.com>
2017-05-08 18:51:30 +0800

    Initial Commit

---
+++ OXHanoiDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OXHanoiDemo
+//
+//  Created by Cloudox on 2017/5/8.
+//  Copyright © 2017年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
